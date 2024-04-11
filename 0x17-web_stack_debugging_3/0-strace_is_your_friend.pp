b552dd1ea17e5eb91c575fefa3c7437f8de94d19
abdallah gamal <abdallah.saleh203@yahoo.com>
2024-04-11 23:02:37 +0200

    Fix typo in sed command to replace "phpp" with "php" in wp-settings.php

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -4,6 +4,7 @@ $file_to_edit = '/var/www/html/wp-settings.php'
 
 #replace line containing "phpp" with "php"
 
+
 exec { 'replace_line':
   command => "sed -i 's/phpp/php/g' ${file_to_edit}",
   path    => ['/bin','/usr/bin']
